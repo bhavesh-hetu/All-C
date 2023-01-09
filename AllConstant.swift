2bbde52c24ab3a672bac8e0bf2b50e808983d8c0
Bhavesh <Kachhadiya101@gmail.com>
2023-01-09 09:49:18 +0530

    test

--- AllConstant.swift
+++ AllConstant.swift
@@ -8,8 +8,14 @@
 import Foundation
 import CoreData
 import Alamofire
+import SDWebImage
 
+//
+//  pod 'Alamofire'
+//pod 'IQKeyboardManager'
+//pod 'SDWebImage'
 
+//
 /// CoreDataManager   ///////////////
 
 class CoreDataManager {
@@ -788,4 +794,35 @@ extension UserDetailViewController: UserDetailDelegate {
         alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
+}
+
+
+
+class UserListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if let _ = self.userImageView {
+            self.userImageView.setCornerRadius(cornerRadius: self.userImageView.frame.size.height / 2.0)
+        }
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupCell(user: Users) {
+        self.userNameLabel.text = user.fullName ?? ""
+        self.userEmailLabel.text = user.email ?? ""
+        self.dateLabel.text = user.updatedAt ?? ""
+        self.userImageView.sd_setImage(with: URL(string: user.profilePicUrl ?? "")!, placeholderImage: UIImage(named: "profilePlaceHolder"), options: .fromCacheOnly, context: nil)
+    }
+    
 }
